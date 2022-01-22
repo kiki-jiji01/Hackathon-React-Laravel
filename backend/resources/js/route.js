922ba09bcaea60eca784e16637f1f4766a4ca38e
genta <yasokawag@gmmail.com>
2022-01-22 20:12:43 +0900

    dom

--- backend/resources/js/route.js
+++ backend/resources/js/route.js
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import {
-//     BrowserRouter,
-//     Route,
-//     Router,
-//     Switch,
-//   } from 'react-router-dom';
-// import Home from './components/Home';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter,
+    Route,
+    Router,
+    Switch,
+  } from 'react-router-dom';
+import Home from './components/Home';
 
-//   function App() {
-//     return (
-//         <div>
-//             <p>jjjj</p>
-//             <Router>
-//                 <Switch>
-//                     <Route path="/"> <Home/> </Route>
-//                 </Switch>
-//             </Router>
+  function App() {
+    return (
+        <div>
+            <p>jjjj</p>
+            <Router>
+                <Switch>
+                    <Route path=""> <Home/> </Route>
+                </Switch>
+            </Router>
             
             
-//         </div>
-//     );
-// }
+        </div>
+    );
+}
 
-//   ReactDOM.render((
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   ), document.getElementById('app'))
+  ReactDOM.render((
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  ), document.getElementById('app'))
