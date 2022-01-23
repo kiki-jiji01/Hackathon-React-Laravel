b8e8efc692a7077b4b3c94016ec786d80301ad74
Ibuki-Narukawa <ibuki0212job@gmail.com>
2022-01-23 11:52:49 +0900

    laravel側でshow()でchatRoom詳細の取得を実装　新規登録機能のバックエンド実装

--- backend/app/Http/Controllers/Api/ChatRoomController.php
+++ backend/app/Http/Controllers/Api/ChatRoomController.php
@@ -6,12 +6,30 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\User;
 use App\Model\ChatRoom;
+use App\Model\ChatMessage;
+use App\Model\Tag;
 
 class ChatRoomController extends Controller
 {
     public function index()
     {
         $chatRooms = ChatRoom::all();
-        return response()->json($posts, 200);
+        return response()->json($chatRooms, 200);
+    }
+
+    public function show(Request $request){
+        $chatRoom = ChatRoom::find($request->id);
+        $chatMessages = ChatMessage::with('chatRoom')->with('user')->where('chat_room_id','=',$request->id)->orderBy('created_at', 'ASC')->get();
+        return response()->json([$chatRoom, $chatMessages], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $chatRoom = new ChatRoom;
+        $chatRoom->title = $request->title;
+        $chatRoom->body = $request->body;
+        $chatRoom->user_id = Auth::id();
+        $chatRoom->save();
+        return response()->json($chatRoom, 200);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::group(['middleware' => 'api'], function(){
-    Route::get('chatRooms', 'App\Http\Controllers\Api\ChatRoomController@index');
-    
+    Route::get('/', 'App\Http\Controllers\Api\ChatRoomController@index');
+    Route::get('/chatRooms/{id}', 'App\Http\Controllers\Api\ChatRoomController@show');
+    Route::post('/create', 'App\Http\Controllers\Api\ChatRoomController@store');
 });
