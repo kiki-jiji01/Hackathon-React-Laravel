aa1ea1384d7e91b4548f3689d502cb4414ff392c
genta <yasokawag@gmmail.com>
2022-01-26 22:56:49 +0900

    #20

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
     <title>Hello World</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+    <script src="{{ mix('/js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
